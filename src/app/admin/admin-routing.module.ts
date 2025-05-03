97712ee70eed8357f48a6fdc435c309609b84b45
Alexis Poaquiza <apoaquiza6287@uta.edu.ec>
2025-05-03 18:08:59 -0500

    Desarrollo frontend del componente RegistroAsesores

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { RegistroAsesoresComponent } from './components/registro-asesores/registro-asesores.component';
 
-const routes: Routes = [{ path: '', component: AdminComponent }];
+const routes: Routes = [
+  { path: '', component: AdminComponent },
+  { path : 'asesores', component: RegistroAsesoresComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,13 @@ import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
-    path: '', component: HomeComponent, data: { breadcrumb: 'Inicio'}
+    path: '', component: HomeComponent, data: { breadcrumb: 'Inicio' }
   },
-  {
-    path:'pages',
-    children:[
-      {path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
-      {path: 'servicios', loadChildren: () => import('./pages/servicios/servicios.module').then(m => m.ServiciosModule)},
-      {path: 'creditos', loadChildren: () => import('./pages/creditos/creditos.module').then(m => m.CreditosModule)},
-      {path: 'inversiones', loadChildren: () => import('./pages/inversiones/inversiones.module').then(m => m.InversionesModule)}
-    ]
-  },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'servicios', loadChildren: () => import('./pages/servicios/servicios.module').then(m => m.ServiciosModule) },
+  { path: 'creditos', loadChildren: () => import('./pages/creditos/creditos.module').then(m => m.CreditosModule) },
+  { path: 'inversiones', loadChildren: () => import('./pages/inversiones/inversiones.module').then(m => m.InversionesModule) },
+
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
   { path: 'asesor', loadChildren: () => import('./asesor/asesor.module').then(m => m.AsesorModule) },
